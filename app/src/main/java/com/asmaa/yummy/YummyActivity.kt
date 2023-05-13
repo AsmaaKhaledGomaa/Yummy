de7e71cbdb31771ecacbe526e0ed239174dfa034
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2023-05-13 23:01:05 +0300

    Update Gradle

--- app/src/main/java/com/asmaa/yummy/YummyActivity.kt
+++ app/src/main/java/com/asmaa/yummy/YummyActivity.kt
@@ -3,7 +3,7 @@ package com.asmaa.yummy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class YummyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
