d87aa3cad13b499a6f0df2498c9de8d648b33f79
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2023-06-29 14:35:26 +0300

    RecipesViewModel

--- app/src/main/java/com/asmaa/yummy/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/asmaa/yummy/viewmodels/RecipesViewModel.kt
@@ -2,6 +2,8 @@ package com.asmaa.yummy.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.asmaa.yummy.data.network.DataStoreRepositry
 import com.asmaa.yummy.util.Constants
 import com.asmaa.yummy.util.Constants.Companion.API_KEY
 import com.asmaa.yummy.util.Constants.Companion.DEFAULT_DIET_TYPE
@@ -13,17 +15,43 @@ import com.asmaa.yummy.util.Constants.Companion.QUERY_DIET
 import com.asmaa.yummy.util.Constants.Companion.QUERY_FILL_INGREDIENTS
 import com.asmaa.yummy.util.Constants.Companion.QUERY_NUMBER
 import com.asmaa.yummy.util.Constants.Companion.QUERY_TYPE
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class RecipesViewModel(application: Application):AndroidViewModel(application) {
+@HiltViewModel
+class RecipesViewModel @Inject constructor(application: Application, private val dataStoreRepository :DataStoreRepositry):AndroidViewModel(application) {
 
-     fun applyQueries(): HashMap<String , String>{
-        val queries: HashMap<String , String> = HashMap()
+    private var mealType = DEFAULT_MEAL_TYPE
+    private var dietType = DEFAULT_DIET_TYPE
+
+    val readMealAndDietType = dataStoreRepository.readMealAndDietType
+
+    fun saveMealAndDietType(mealType: String, mealTypeId: Int, dietType: String, dietTypeId: Int) =
+        viewModelScope.launch(Dispatchers.IO) {
+            dataStoreRepository.saveMealAndDietType(mealType, mealTypeId, dietType, dietTypeId)
+        }
+
+    fun applyQueries(): HashMap<String, String> {
+        val queries: HashMap<String, String> = HashMap()
+
+        viewModelScope.launch {
+            readMealAndDietType.collect { value ->
+                mealType = value.selectedMealType
+                dietType = value.selectedDietType
+            }
+        }
+
         queries[QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
         queries[QUERY_API_KEY] = API_KEY
-        queries[QUERY_TYPE] = DEFAULT_MEAL_TYPE
-        queries[QUERY_DIET] = DEFAULT_DIET_TYPE
+        queries[QUERY_TYPE] = mealType
+        queries[QUERY_DIET] = dietType
         queries[QUERY_ADD_RECIPE_INFORMATION] = "true"
         queries[QUERY_FILL_INGREDIENTS] = "true"
+
         return queries
     }
+
 }
