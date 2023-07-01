ddcfbf243983653fd2d760a71832dfcdd9c6f90f
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2023-07-02 00:28:18 +0300

    RecipesViewModel

--- app/src/main/java/com/asmaa/yummy/ui/fragment/recipes/bottomsheet/RecipesBottomSheetFragment.kt
+++ app/src/main/java/com/asmaa/yummy/ui/fragment/recipes/bottomsheet/RecipesBottomSheetFragment.kt
@@ -1,6 +1,7 @@
 package com.asmaa.yummy.ui.fragment.recipes.bottomsheet
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -17,6 +18,7 @@ import com.asmaa.yummy.util.Constants.Companion.DEFAULT_MEAL_TYPE
 import com.asmaa.yummy.viewmodels.RecipesViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import java.util.Locale
 
 class RecipesBottomSheetFragment : BottomSheetDialogFragment(){
@@ -42,10 +44,12 @@ class RecipesBottomSheetFragment : BottomSheetDialogFragment(){
         // Inflate the layout for this fragment
         _binding = RecipesBottomSheetBinding.inflate(inflater, container, false)
 
-        recipesViewModel.readMealAndDietType.asLiveData().observe(viewLifecycleOwner,{value ->
+        recipesViewModel.readMealAndDietType.asLiveData().observe(viewLifecycleOwner) { value ->
             mealTypeChip = value.selectedMealType
             dietTypeChip = value.selectedDietType
-        })
+            updateChip(value.selectedMealTypeId, binding.mealTypeChipGroup)
+            updateChip(value.selectedDietTypeId, binding.dietTypeChipGroup)
+        }
         binding.mealTypeChipGroup.setOnCheckedChangeListener { group, selectedChipId ->
             val chip = group.findViewById<Chip>(selectedChipId)
             val selectedMealType = chip.text.toString().toLowerCase(Locale.ROOT)
@@ -67,8 +71,13 @@ class RecipesBottomSheetFragment : BottomSheetDialogFragment(){
         return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun updateChip(chipId: Int, chipGroup: ChipGroup) {
+        if (chipId != 0) {
+            try {
+                chipGroup.findViewById<Chip>(chipId).isChecked = true
+            } catch (e: Exception) {
+                Log.d("RecipesBottomSheet", e.message.toString())
+            }
+        }
     }
 }
