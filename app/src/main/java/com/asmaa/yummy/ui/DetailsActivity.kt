bae8679022af4a3e61229d8feb1e8b1dbd3b9934
Asmaa Khaled Gomaa <asmaa.kh.gomaa@gmail.com>
2023-07-18 02:38:03 +0300

    PagerAdapter DetailsActivity

--- app/src/main/java/com/asmaa/yummy/ui/DetailsActivity.kt
+++ app/src/main/java/com/asmaa/yummy/ui/DetailsActivity.kt
@@ -5,12 +5,19 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.navArgs
 import com.asmaa.yummy.R
+import com.asmaa.yummy.adapters.PagerAdapter
 import com.asmaa.yummy.databinding.ActivityDetailsBinding
+import com.asmaa.yummy.ui.fragment.ingredients.IngredientsFragment
+import com.asmaa.yummy.ui.fragment.instructions.InstructionsFragment
+import com.asmaa.yummy.ui.fragment.overview.OverviewFragment
 
 class DetailsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailsBinding
+    private val args by navArgs<DetailsActivityArgs>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailsBinding.inflate(layoutInflater)
@@ -21,6 +28,27 @@ class DetailsActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val fragments = ArrayList<Fragment>()
+        fragments.add(OverviewFragment())
+        fragments.add(IngredientsFragment())
+        fragments.add(InstructionsFragment())
+
+        val titles = ArrayList<String>()
+        titles.add("Overview")
+        titles.add("Ingredients")
+        titles.add("Instructions")
+
+        val resultBundle = Bundle()
+        resultBundle.putParcelable("recipesBundle", args.result)
+
+        val adapter = PagerAdapter(
+            resultBundle,
+            fragments,
+            titles,
+            supportFragmentManager
+        )
+        binding.viewPager.adapter = adapter
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
